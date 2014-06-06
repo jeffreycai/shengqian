684b1dbb4c88b0a5692d95fdaa02ec8357c0fe35
pdrupal <pdrupal@Jeffreyweb.(none)>
2014-06-06 05:06:21 -0400

    work

--- includes/templates/frontend/homepage.tpl.php
+++ includes/templates/frontend/homepage.tpl.php
@@ -35,6 +35,7 @@
       </ul>
     </div>
   </div>
+  
   <h3 id="food">美食折扣 - 吃货省钱 <span><a href="#" role="button" data-goto="promoted">&raquo; 精选</a> <a href="#" role="button" data-goto="goods">&raquo; 商品</a> <a href="#" role="button" data-goto="travel">&raquo; 旅游</a> <a href="#" role="button" data-goto="event">&raquo; 活动</a></span></h3>
   <div class="row">
     <?php foreach (Deal::findAllByCategory('food', 12) as $deal): ?>
@@ -65,6 +66,40 @@
     <div class="col-xs-12">
       <ul class="pager">
         <li class="next"><a href="/deals/food">更多美食折扣 &RightArrow;</a></li>
+      </ul>
+    </div>
+  </div>
+  
+  <h3 id="event">活动折扣 - 玩货省钱 <span><a href="#" role="button" data-goto="promoted">&raquo; 精选</a> <a href="#" role="button" data-goto="food">&raquo; 美食</a> <a href="#" role="button" data-goto="goods">&raquo; 商品</a> <a href="#" role="button" data-goto="travel">&raquo; 旅游</a></span></h3>
+  <div class="row">
+    <?php foreach (Deal::findAllByCategory('event', 12) as $deal): ?>
+      <div class="col-sm-4 col-md-3">
+        <div class="card">
+          <div class="row">
+            <div class="col-xs-5 col-xs-push-7 col-sm-12 col-sm-push-0">
+              <div class="tn">
+                <img class="img-responsive" src="<?php echo $deal->getThumbnail($conf['deal']['thumbnail_size']) ?>" alt="<?php echo html_entity_decode($deal->getTitle()); ?>" />
+              </div>
+            </div>
+            <div class="col-xs-7 col-xs-pull-5 col-sm-12 col-sm-pull-0">
+              <h4><a href="<?php echo $deal->getPageUrl(false); ?>"><?php echo $deal->getTitle(); ?></a></h4>
+            </div>
+          </div>
+          <div class="tags">
+            <span class="label label-primary"><?php echo $deal->getCategory(); ?></span>
+            <?php if ($deal->getSaving()): ?>
+              <span class="label label-danger">省<?php echo $deal->getSaving(); ?></span>
+            <?php endif; ?>
+            <?php if ($deal->getDiscount()): ?>
+              <span class="label label-warning"><?php echo $deal->getDiscount(); ?>折扣</span>
+            <?php endif; ?>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+    <div class="col-xs-12">
+      <ul class="pager">
+        <li class="next"><a href="/deals/event">更多活动折扣 &RightArrow;</a></li>
       </ul>
     </div>
   </div>
@@ -133,40 +168,6 @@
     <div class="col-xs-12">
       <ul class="pager">
         <li class="next"><a href="/deals/travel">更多旅游折扣 &RightArrow;</a></li>
-      </ul>
-    </div>
-  </div>
-  
-  <h3 id="event">活动折扣 - 玩货省钱 <span><a href="#" role="button" data-goto="promoted">&raquo; 精选</a> <a href="#" role="button" data-goto="food">&raquo; 美食</a> <a href="#" role="button" data-goto="goods">&raquo; 商品</a> <a href="#" role="button" data-goto="travel">&raquo; 旅游</a></span></h3>
-  <div class="row">
-    <?php foreach (Deal::findAllByCategory('event', 12) as $deal): ?>
-      <div class="col-sm-4 col-md-3">
-        <div class="card">
-          <div class="row">
-            <div class="col-xs-5 col-xs-push-7 col-sm-12 col-sm-push-0">
-              <div class="tn">
-                <img class="img-responsive" src="<?php echo $deal->getThumbnail($conf['deal']['thumbnail_size']) ?>" alt="<?php echo html_entity_decode($deal->getTitle()); ?>" />
-              </div>
-            </div>
-            <div class="col-xs-7 col-xs-pull-5 col-sm-12 col-sm-pull-0">
-              <h4><a href="<?php echo $deal->getPageUrl(false); ?>"><?php echo $deal->getTitle(); ?></a></h4>
-            </div>
-          </div>
-          <div class="tags">
-            <span class="label label-primary"><?php echo $deal->getCategory(); ?></span>
-            <?php if ($deal->getSaving()): ?>
-              <span class="label label-danger">省<?php echo $deal->getSaving(); ?></span>
-            <?php endif; ?>
-            <?php if ($deal->getDiscount()): ?>
-              <span class="label label-warning"><?php echo $deal->getDiscount(); ?>折扣</span>
-            <?php endif; ?>
-          </div>
-        </div>
-      </div>
-    <?php endforeach; ?>
-    <div class="col-xs-12">
-      <ul class="pager">
-        <li class="next"><a href="/deals/event">更多活动折扣 &RightArrow;</a></li>
       </ul>
     </div>
   </div>
